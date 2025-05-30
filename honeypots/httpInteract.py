91be38a0e53f7003314883aeb7c5dafbe64212b5
Rocco Sicilia <rocco.sicilia@protonmail.com>
2025-05-30 22:05:13 +0200

    test print b

--- honeypots/httpInteract.py
+++ honeypots/httpInteract.py
@@ -17,7 +17,7 @@ class RequestLoggerHandler(BaseHTTPRequestHandler):
         self.log_request_to_file()
 
         if self.path == "/admin": # Respond with Admin Page
-            with open("template_login.html", "r", encoding="utf-8") as f:
+            with open("template_login.html", "rb", encoding="utf-8") as f:
                 content = f.read()
             self.send_response(200)
             self.send_header("Content-type", "text/html")
